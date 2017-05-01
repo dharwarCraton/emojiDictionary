f6426ed1355aa239a2f1591bb4d253fe15828e83
dharwarCraton <fnalam@bsugmail.net>
2017-05-01 14:00:20 -0400

    Added tableview array and working on 2nd screen (storyboard only at this point).

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var daFirstTableView: UITableView!
+    
+    var emojisArray = ["🙃", "💩", "👠", "🐹", "🐬", "😻", "☂️"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        daFirstTableView.dataSource = self
+        daFirstTableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojisArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojisArray[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
